fb31f24a844dddbea6accd7a14d8475aab841460
Zoltán Balázs <zoltanbalazsacademic@gmail.com>
2021-08-07 21:52:16 +0200

    // As it turns out i need a SolutionFile to be able to reliably evoke the intellisense in VSCode if i reference a different project

---
+++ BackendTests/NaturalDNAControllerTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using Backend;
+using Backend.Controllers;
+
+namespace BackendTests
+{
+	public class NaturalDNAControllerTests
+	{
+		[Fact]
+		public void GetAsync_Returns_Correct_NumberOfEntries()
+		{
+
+			//NaturalDNAController controller = new NaturalDNAController();
+
+		}
+	}
+}
