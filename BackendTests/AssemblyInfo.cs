0996c4d3841c8fe7c658401f758dbaf1ef32c602
Zoltán Balázs <zoltanbalazsacademic@gmail.com>
2021-08-09 03:44:48 +0200

    // Now we can order the Suits

--- BackendTests/AssemblyInfo.cs
+++ BackendTests/AssemblyInfo.cs
@@ -6,3 +6,5 @@ using Xunit;
 [assembly: TestCaseOrderer("Xunit.Extensions.Ordering.TestCaseOrderer", "Xunit.Extensions.Ordering")]
 //Optional
 [assembly: TestCollectionOrderer("Xunit.Extensions.Ordering.CollectionOrderer", "Xunit.Extensions.Ordering")]
+// You can order test classes in collections
+[assembly: TestFramework("Xunit.Extensions.Ordering.TestFramework", "Xunit.Extensions.Ordering")]
