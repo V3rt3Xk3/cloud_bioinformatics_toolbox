7a808579c9cffdd3ad6a4dd9f00595ff41cae14d
Zoltán Balázs <zoltanbalazsacademic@gmail.com>
2021-09-01 04:40:01 +0200

    // Setup the SuitSetups with the ITestSuite interface

---
+++ _BackendTests/BackendAPI_Mongo_IntegrationTests/UserControllerTests.cs
@@ -0,0 +1,59 @@
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using MongoDB.Driver;
+
+using Xunit;
+using Xunit.Extensions.Ordering;
+
+using Backend.Models;
+using Backend.Models.UserManagement;
+using BackendTests.Utilities;
+
+namespace BackendTests
+{
+	public class UserControllerTests : IClassFixture<CustomWebApplicationFactory<Backend.Startup>>, ITestSuite
+	{
+		private readonly CustomWebApplicationFactory<Backend.Startup> _factory;
+		private MongoClient _mongoClient;
+		private string _accessToken;
+		private readonly string _dbName;
+		private readonly string _usersCollectionName;
+		private readonly string _naturalDNACollectionName;
+		public UserControllerTests(CustomWebApplicationFactory<Backend.Startup> factory)
+		{
+			this._factory = factory;
+
+			string connectionString = "mongodb://cloud_bioinformaitcs_mongo_dev:%2333FalleN666%23@localhost:27017/?authSource=cloud_bioinformatics_test";
+
+			this._mongoClient = new(connectionString);
+			this._dbName = "cloud_bioinformatics_test";
+			this._usersCollectionName = "Users";
+			// this._naturalDNACollectionName = "NaturalDNASequences";
+		}
+
+		[Fact, Order(1)]
+		public async Task TestSuiteSetUp()
+		{
+			// Suite Setup
+			ITestSuite.MongoDBCleanUp(this._mongoClient);
+			this._accessToken = await ITestSuite.MongoDBRegisterAndAuthenticate(this._factory);
+
+			// Arrange
+			HttpClient client = _factory.CreateClient();
+			HttpRequestMessage requestMessage = new(HttpMethod.Get, "/api/users");
+			requestMessage.Headers.Add("Authorization", this._accessToken);
+			HttpResponseMessage response = await client.SendAsync(requestMessage);
+			response.EnsureSuccessStatusCode(); // Status code 200-299
+
+
+
+			// Assert
+			string errorMessage = $"Couldn't get Users. - We got StatusCode {response.StatusCode}";
+			AssertX.Equal(200, (int)response.StatusCode, errorMessage);
+		}
+	}
+}
--- _BackendTests/Utilities/ITestSuite.cs
+++ _BackendTests/Utilities/ITestSuite.cs
@@ -1,6 +1,12 @@
-using System;
+using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 using MongoDB.Driver;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Backend.Models;
+using Backend.Models.UserManagement;
 
 namespace BackendTests.Utilities
 {
@@ -9,24 +15,54 @@ namespace BackendTests.Utilities
 
 		Task TestSuiteSetUp();
 
-		/// <summary>
-		/// Cleans up the MongoDB "cloud_bioinformatics_test" test database. | Drops it.
-		/// </summary>
-		public static void TestCase_DatabaseCleanUp()
+		public static async Task<string> MongoDBRegisterAndAuthenticate(CustomWebApplicationFactory<Backend.Startup> _factory)
 		{
-			ITestSuite.MongoDBCleanUp();
-		}
-		// NOTE: As it turns out, Interfaces can have their own static implementations as well.
-		/// <summary>
-		/// Cleans up the MongoDB "cloud_bioinformatics_test" test database. | Drops it.
-		/// </summary>
-		public static void MongoDBCleanUp()
-		{
-			string connectionString = "mongodb://cloud_bioinformaitcs_mongo_dev:%2333FalleN666%23@localhost:27017/?authSource=cloud_bioinformatics_test";
-			MongoClient client = new(connectionString);
+			// Arrange
+			HttpClient client = _factory.CreateClient();
+			HttpResponseMessage response;
 
-			string DatabaseNamespace = "cloud_bioinformatics_test";
-			client.DropDatabase(DatabaseNamespace);
+			// Register
+			RegisterRequest registerRequest = new()
+			{
+				Username = "vertex",
+				Password = "#33FalleN666#",
+				RePassword = "33FalleN666"
+			};
+			StringContent registerJSONContent = new(JsonConvert.SerializeObject(registerRequest), Encoding.UTF8, "application/json");
+			registerJSONContent.Headers.Add("X-Forwarded-For", "127.0.0.1");
+			response = await client.PostAsync("/api/users/register", registerJSONContent);
+			response.EnsureSuccessStatusCode(); // Status code 200-299
+
+			// Authenticate
+			AuthenticateRequest authenticateRequest = new()
+			{
+				Username = "vertex",
+				Password = "#33FalleN666#"
+			};
+			StringContent authenticateJSONContent = new(JsonConvert.SerializeObject(authenticateRequest), Encoding.UTF8, "application/json");
+			authenticateJSONContent.Headers.Add("X-Forwarded-For", "127.0.0.1");
+			response = await client.PostAsync("/api/users/authenticate", authenticateJSONContent);
+			response.EnsureSuccessStatusCode(); // Status code 200-299
+			string responseString = response.Content.ReadAsStringAsync().Result;
+			JObject jsonResponse = JObject.Parse(responseString);
+			string accessToken = (string)jsonResponse["AccessToken"];
+
+			return accessToken;
+		}
+
+		/// <summary>
+		/// Cleans up the MongoDB "cloud_bioinformatics_test" test database. | Drops it.
+		/// </summary>
+		public static void MongoDBCleanUp(MongoClient _client, string _dbName = "cloud_bioinformatics_test")
+		{
+			_client.DropDatabase(_dbName);
+		}
+		public static void MongoDBCollectionCleanup<TClass>(MongoClient _client, string _collectionName, string _dbName = "cloud_bioinformatics_test")
+		{
+			IMongoDatabase dataBase = _client.GetDatabase(_dbName);
+			IMongoCollection<TClass> userCollection = dataBase.GetCollection<TClass>(_collectionName);
+
+			userCollection.DeleteMany(Builders<TClass>.Filter.Empty);
 		}
 	}
 }
