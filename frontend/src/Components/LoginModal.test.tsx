7b68f94bca8376988be876c3908577c68acc1dd8
Zoltán Balázs <zoltanbalazsacademic@gmail.com>
2021-09-05 07:14:13 +0200

    // This is how you share a resource between all the tests in a file.

--- frontend/src/Components/LoginModal.test.tsx
+++ frontend/src/Components/LoginModal.test.tsx
@@ -2,39 +2,34 @@ import React from "react";
 import LoginModal from "./LoginModal";
 import { fireEvent, render } from "@testing-library/react";
 
-// FIXME: Remove repeats of code. I mean particularly the lambdaFunction.
+// Shared variables:
+const emptyFunction = () => { };
+
+// Actual tests
 describe("LoginModal Unit tests", () => {
 	test("Header renders correctly with correct content", () => {
-		const lambdaFunction = () => { };
-
-		const component = render(<LoginModal show={true} onClose={lambdaFunction} />);
+		const component = render(<LoginModal show={true} onClose={emptyFunction} />);
 		const loginHeader = component.getByTestId("loginModalHeader");
 
 		expect(loginHeader.textContent).toBe("Login");
 	});
 
 	test("Username input has placeholder 'Username'", () => {
-		const lambdaFunction = () => { };
-
-		const component = render(<LoginModal show={true} onClose={lambdaFunction} />);
+		const component = render(<LoginModal show={true} onClose={emptyFunction} />);
 		const usernameInputElement = component.getByTestId("usernameInput");
 
 		expect(usernameInputElement.getAttribute("placeholder")).toBe("Username");
 	});
 
 	test("Username input has initial value ''", () => {
-		const lambdaFunction = () => { };
-
-		const component = render(<LoginModal show={true} onClose={lambdaFunction} />);
+		const component = render(<LoginModal show={true} onClose={emptyFunction} />);
 		const usernameInputElement = component.getByTestId("usernameInput");
 
 		expect(usernameInputElement.getAttribute("value")).toBe("");
 	});
 
 	test("Username input element can change its value!", () => {
-		const lambdaFunction = () => { };
-
-		const component = render(<LoginModal show={true} onClose={lambdaFunction} />);
+		const component = render(<LoginModal show={true} onClose={emptyFunction} />);
 		const usernameInputElement = component.getByTestId("usernameInput");
 
 		expect(usernameInputElement.getAttribute("value")).toBe("");
@@ -51,27 +46,21 @@ describe("LoginModal Unit tests", () => {
 
 
 	test("Password input has placeholder 'Password'", () => {
-		const lambdaFunction = () => { };
-
-		const component = render(<LoginModal show={true} onClose={lambdaFunction} />);
+		const component = render(<LoginModal show={true} onClose={emptyFunction} />);
 		const passwordInputElement = component.getByTestId("passwordInput");
 
 		expect(passwordInputElement.getAttribute("placeholder")).toBe("Password");
 	});
 
 	test("Password input has initial value ''", () => {
-		const lambdaFunction = () => { };
-
-		const component = render(<LoginModal show={true} onClose={lambdaFunction} />);
+		const component = render(<LoginModal show={true} onClose={emptyFunction} />);
 		const passwordInputElement = component.getByTestId("passwordInput");
 
 		expect(passwordInputElement.getAttribute("value")).toBe("");
 	});
 
 	test("Password input element can change its value!", () => {
-		const lambdaFunction = () => { };
-
-		const component = render(<LoginModal show={true} onClose={lambdaFunction} />);
+		const component = render(<LoginModal show={true} onClose={emptyFunction} />);
 		const passwordInputElement = component.getByTestId("passwordInput");
 
 		expect(passwordInputElement.getAttribute("value")).toBe("");
